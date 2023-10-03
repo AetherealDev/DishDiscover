221d0598d2eae29fe3a73e4387f8dd8ac6a65569
AetherealDev <131472446+AetherealDev@users.noreply.github.com>
2023-10-02 21:12:35 -0400

    user model
    added a user model and an empty restaurant model

---
+++ server/models/User.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+const restaurantSchema = require('./Restaurant');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    // savedRestaurants will be an array of the Restaurant schema
+    savedRestaurants: [restaurantSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// hash user password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+// virtual to count saved restaurants
+userSchema.virtual('restaurantCount').get(function () {
+  return this.savedRestaurants.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
