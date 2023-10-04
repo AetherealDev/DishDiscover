e73ac516c929c65bcca964070cd60264185827a8
AetherealDev <131472446+AetherealDev@users.noreply.github.com>
2023-10-04 18:58:52 -0400

    Added me query resolver
    Added me query resolver, and exported the user model from the models directory

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,16 @@
 const { User } = require('../models');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password') // exclude the __v and password fields
+                return userData;
+            }
+            throw AuthenticationError;
+        },
+    },
+    Mutation: {
+    },
+};
