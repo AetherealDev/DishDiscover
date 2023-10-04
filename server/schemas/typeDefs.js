f9b58fb9580d00bc5477e5da5633e2c8df68310c
AetherealDev <131472446+AetherealDev@users.noreply.github.com>
2023-10-04 18:53:11 -0400

    Added typeDefs
    Added typeDefs to schema, starting to work on resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,52 @@
+const typeDefs = `
+type User {
+  _id: ID
+  username: String
+  email: String
+  restaurantCount: Int
+  savedRestaurants: [Book]
+}
+
+type Restraunt{
+    restaurantId: String
+    name: String
+    image: String
+    url: String
+    location: String
+    price: String
+    rating: String
+    phone: String
+    review_count: String
+    categories: String
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+type Query {
+  me: User
+}
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  saveRestaurant(input: RestaurantInput): User
+  removeRestaurant(restaurantId: String!): User
+}
+
+input RestaurantInput {
+    restaurantId: String
+    name: String
+    image: String
+    url: String
+    location: String
+    price: String
+    rating: String
+    phone: String
+    review_count: String
+    categories: String
+}
+
+`
