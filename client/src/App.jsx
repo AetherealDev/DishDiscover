7e568afeceb15232ca37f9ed06f30550fa0c0d9c
AetherealDev <131472446+AetherealDev@users.noreply.github.com>
2023-10-11 17:22:48 -0400

    added axios
    added axios and made an api request to google places

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,8 @@
 import './App.css'
 import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
-
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 
 import {
   ApolloClient,
@@ -34,9 +32,9 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-function App() {
-  // const [count, setCount] = useState(0)
 
+
+function App() {
       return (
         <ApolloProvider client={client}>
           <div className="flex-column justify-flex-start min-100-vh">
---
+++ client/src/utils/API.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const getNearbyRestaurants = async () => {
+    const params = {
+        location: '40.7128,-74.0060',
+        radius: 500,
+        type: 'restaurant',
+        key: 'YOUR_API_KEY_HERE'
+    };
+
+    const response = await axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', { params });
+    console.log(response.data);
+    return response.data;
+};
