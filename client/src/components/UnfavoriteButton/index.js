843c95ba250d9ca0f714521661a63067a0b63ec9
AetherealDev <131472446+AetherealDev@users.noreply.github.com>
2023-10-14 23:26:55 -0400

    Create Unfavorite Button
    create an initial unfavorite button

---
+++ client/src/components/UnfavoriteButton/index.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { REMOVE_RESTAURANT} from '../../utils/mutations';
+import { QUERY_ME } from '../../utils/queries';
+import { Button } from 'react-bootstrap';
+
+
+const UnfavoriteButton = ({ restaurantId }) => {
+
+    const [removeRestaurant] = useMutation(REMOVE_RESTAURANT, {
+        update(cache, { data: { removeRestaurant } }) {
+        try {
+            cache.writeQuery({
+            query: QUERY_ME,
+            data: { me: removeRestaurant },
+            });
+        } catch (e) {
+            console.error(e);
+        }
+        },
+    });
+    
+    const handleRemoveRestaurant = async (restaurantId) => {
+        try {
+        const { data } = await removeRestaurant({
+            variables: { restaurantId },
+        });
+        } catch (err) {
+        console.error(err);
+        }
+    };
+    
+    return (
+        <Button
+        className="btn-block btn-danger"
+        onClick={() => handleRemoveRestaurant(restaurantId)}
+        >
+        Remove this Restaurant
+        </Button>
+    );
+    }
